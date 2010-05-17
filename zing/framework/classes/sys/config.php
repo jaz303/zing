52451fcfc9d79f6c4ffccc328ef0bc44b575db0d
Jason Frame <jason@magiclamp.co.uk>
2010-05-18 00:18:01 +0100

    system config method for adding app config programatically

--- zing/framework/classes/sys/config.php
+++ zing/framework/classes/sys/config.php
@@ -16,5 +16,21 @@ class Config
         
         file_put_contents($file, $contents);
     }
+    
+    public static function write_application_config_code($code, $config_file = 'main') {
+        
+        $path = ZING_CONFIG_DIR . '/app/' . $config_file . '.php';
+        
+        $source  = file_get_contents($path);
+        $source  = trim(preg_replace('|\?\>\s*$|m', '', $source));
+        $source .= "\n\n" . trim($code) . "\n?>";
+        
+        file_put_contents($path, $source);
+        
+    }
+    
+    public static function write_application_config_assignment($variable, $value, $config_file = 'main') {
+        self::write_application_config_code("$variable = " . var_export($value, true) . ";\n", $config_file);
+    }
 }
 ?>
