dd3a179023c3af4f52e7d0700699b56a053eb765
Jason Frame <jason@magiclamp.co.uk>
2010-05-31 10:47:23 +0100

    default plugin locator is now configurable

--- zing/config/app/system.php
+++ zing/config/app/system.php
@@ -1,5 +1,10 @@
 <?php
 /**
+ * Class used to locate plugins
+ */
+$_ZING['zing.plugin.locator'] = 'zing\\plugin\\DefaultLocator';
+
+/**
  * Class used to locate generators
  */
 $_ZING['zing.generator.locator'] = 'zing\\generator\\DefaultLocator';
