3473d54b7f2fbc41479106e6d3abba36a4115172
Jason Frame <jason@onehackoranother.com>
2011-03-01 20:19:16 +0000

    HTML5'ing it up

--- zing/app/views/layouts/application.html.php
+++ zing/app/views/layouts/application.html.php
@@ -1,29 +1,52 @@
-<!DOCTYPE html>
+<!doctype html>
 
-<!--[if lt IE 7]><html class="ie ie6 lte9 lte8 lte7"><![endif]-->
-<!--[if IE 7]><html class="ie ie7 lte9 lte8 lte7"><![endif]-->
-<!--[if IE 8]><html class="ie ie8 lte9 lte8"> <![endif]-->
-<!--[if IE 9]><html class="ie ie9 lte9"> <![endif]-->
-<!--[if gt IE 9]><html><![endif]-->
-<!--[if !IE]><!-->
-    
-<html>
+<!--
+  Zing's default application layout is a slightly modified version of HTML5 Boilerplate
+  http://html5boilerplate.com/
+  (c) Paul Irish & contributors: https://github.com/paulirish/html5-boilerplate/contributors
+-->
 
-<!--<![endif]-->
-
+<!--[if lt IE 7 ]> <html class="no-js ie6" lang="en"> <![endif]-->
+<!--[if IE 7 ]>    <html class="no-js ie7" lang="en"> <![endif]-->
+<!--[if IE 8 ]>    <html class="no-js ie8" lang="en"> <![endif]-->
+<!--[if (gte IE 9)|!(IE)]><!--> <html class="no-js" lang="en"> <!--<![endif]-->
 <head>
-<title>Zing! Application</title>
-  
-<?= stylesheet_collection('defaults') ?>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-<?= javascript_collection('defaults') ?>
+  <title>Zing! Application</title>
   
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+  
+  <?= stylesheet_collection('defaults') ?>
+  <?= javascript_include_tag('modernizer-1.7.min.js') ?>
 </head>
 
 <body>
-  
-<?= $this->content_for('layout') ?>
-  
-</body>
 
+  <div id="container">
+    <header>
+
+    </header>
+    <div id="main" role="main">
+      <?= $this->content_for('layout') ?>
+    </div>
+    <footer>
+
+    </footer>
+  </div>
+
+  <?= javascript_collection('defaults') ?>
+
+  <!--[if lt IE 7 ]>
+    <?= javascript_include_tag('js/libs/dd_belatedpng.js') ?>
+    <script>DD_belatedPNG.fix('img, .png_bg');</script>
+  <![endif]-->
+
+</body>
 </html>
