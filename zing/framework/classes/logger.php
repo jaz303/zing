3680202b54f20ebe85322063ecacd1b35d39e26f
Jason Frame <jason@onehackoranother.com>
2011-10-07 12:52:04 +0100

    Logging is colorful

--- zing/framework/classes/logger.php
+++ zing/framework/classes/logger.php
@@ -11,6 +11,17 @@ class Logger
     const ERROR     = 6;
     const FATAL     = 7;
     
+    private static $COLORS = array(
+        0           => "\033[0m",
+        1           => "\033[1m",
+        2           => "\033[1m",
+        3           => "\033[1m",
+        4           => "\033[1m",
+        5           => "\033[1m\033[33m",
+        6           => "\033[1m\033[31m",
+        7           => "\033[1m\033[31m"
+    );
+    
     private static $instance = null;
     public static function instance() {
         if (self::$instance === null) {
@@ -22,8 +33,9 @@ class Logger
     private $file;
     private $fd;
     private $threshold;
+    private $colorize;
     
-    public function __construct($file_or_resource = "php://stderr", $threshold = self::DEBUG) {
+    public function __construct($file_or_resource = "php://stderr", $threshold = self::DEBUG, $colorize = true) {
         if (is_resource($file_or_resource)) {
             $this->file = null;
             $this->td   = $file_or_resource;
@@ -31,10 +43,16 @@ class Logger
             $this->file = $file_or_resource;
             $this->fd   = fopen($file_or_resource, "w");
         }
+        $this->threshold = $threshold;
+        $this->colorize = $colorize;
     }
     
-    public function log($prefix, $thing) {
-        $out = date('Y-m-d\TH:i:s') . ' [' . $prefix . '] ';
+    public function log($level, $prefix, $thing) {
+        $prefix = '[' . $prefix . ']';
+        if ($this->colorize) {
+            $prefix = self::$COLORS[$level] . $prefix . self::$COLORS[0];
+        }
+        $out = date('Y-m-d\TH:i:s') . ' ' . $prefix . ' ';
         if ($thing instanceof \Exception) {
             $msg  = "Exception caught: " . $thing->getMessage() . ' (' . get_class($thing) . ")\n";
             $msg .= "{$thing->getFile()}:{$thing->getLine()}\n";
@@ -48,51 +66,51 @@ class Logger
     }
     
     public function debug($msg, $arg1 = null) {
-        if ($this->threshold < self::DEBUG) {
+        if ($this->threshold <= self::DEBUG) {
             if ($arg1 !== null) $msg = call_user_func_array('sprintf', func_get_args());
-            $this->log("DEBUG", $msg);
+            $this->log(self::DEBUG, "DEBUG", $msg);
         }
     }
     
     public function info($msg, $arg1 = null) {
-        if ($this->threshold < self::INFO) {
+        if ($this->threshold <= self::INFO) {
             if ($arg1 !== null) $msg = call_user_func_array('sprintf', func_get_args());
-            $this->log("INFO", $msg);
+            $this->log(self::INFO, "INFO", $msg);
         }
     }
     
     public function message($msg, $arg1 = null) {
-        if ($this->threshold < self::MESSAGE) {
+        if ($this->threshold <= self::MESSAGE) {
             if ($arg1 !== null) $msg = call_user_func_array('sprintf', func_get_args());
-            $this->log("MSG", $msg);
+            $this->log(self::MESSAGE, "MSG", $msg);
         }
     }
     
     public function notice($msg, $arg1 = null) {
-        if ($this->threshold < self::NOTICE) {
+        if ($this->threshold <= self::NOTICE) {
             if ($arg1 !== null) $msg = call_user_func_array('sprintf', func_get_args());
-            $this->log("NOTICE", $msg);
+            $this->log(self::NOTICE, "NOTICE", $msg);
         }
     }
     
     public function warning($msg, $arg1 = null) {
-        if ($this->threshold < self::WARNING) {
+        if ($this->threshold <= self::WARNING) {
             if ($arg1 !== null) $msg = call_user_func_array('sprintf', func_get_args());
-            $this->log("WARNING", $msg);
+            $this->log(self::WARNING, "WARNING", $msg);
         }
     }
     
     public function error($msg, $arg1 = null) {
-        if ($this->threshold < self::ERROR) {
+        if ($this->threshold <= self::ERROR) {
             if ($arg1 !== null) $msg = call_user_func_array('sprintf', func_get_args());
-            $this->log("ERROR", $msg);
+            $this->log(self::ERROR, "ERROR", $msg);
         }
     }
     
     public function fatal($msg, $arg1 = null) {
-        if ($this->threshold < self::FATAL) {
+        if ($this->threshold <= self::FATAL) {
             if ($arg1 !== null) $msg = call_user_func_array('sprintf', func_get_args());
-            $this->log("FATAL", $msg);
+            $this->log(self::FATAL, "FATAL", $msg);
         }
     }
 }
